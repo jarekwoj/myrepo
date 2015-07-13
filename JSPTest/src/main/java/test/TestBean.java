cffaad980d8a403969587e120061855f7350e85b
wojtalik <Jaroslaw.Wojtalik@tomtom.com>
2015-07-13 09:15:12 +0200

    commit

---
+++ JSPTest/src/main/java/test/TestBean.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TestBean {
+	private String message = "No message specified";
+	 
+	   public String getMessage() {
+	      return(message);
+	   }
+	   public void setMessage(String message) {
+	      this.message = message;
+	   }
+}
